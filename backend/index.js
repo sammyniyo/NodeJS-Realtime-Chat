482e1d273ec6346db4917bd41b56afb20dafaa6f
sammyniyo <samshakul@gmail.com>
2024-03-15 08:57:16 +0200

    front-end

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.post("/authenticate", async (req, res) => {
     const r = await axios.put(
       'https://api.chatengine.io/users/',
       { username: username, secret: username, first_name: username },
-      { headers: { "private_key": "48f1568c-5639-4d1b-a764-c48398a49680"} }
+      { headers: { "private-key": "48f1568c-5639-4d1b-a764-c48398a49680"} }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
